90ad74fdbb8e5f3758c19dcd0826eb22da6cb2b1
Marta Koperek <makoperek@gmail.com>
2019-10-01 22:21:28 +0200

    Add game result

--- js/script.js
+++ js/script.js
@@ -13,3 +13,39 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+	playerMove = 'papier';
+} else if (playerInput == '3'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+	printMessage('Ty przegrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+	printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+	printMessage('Ty przegrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+	printMessage('Ty przegrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+	printMessage('Ty wygrywasz!');
+}else if (computerMove === playerMove) {
+	printMessage('Remis!');
+}else {
+	printMessage("Wpisałeś złą liczbę");
+}
